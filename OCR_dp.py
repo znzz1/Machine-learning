4badfca17e02540d6811ed71fba6658002aeed05
scynz1@nottingham.edu.cn <scynz1@nottingham.edu.cn>
2021-01-31 20:19:45 +0800

    Update OCR_dp.py

--- OCR_dp.py
+++ OCR_dp.py
@@ -2,8 +2,10 @@ import numpy as np
 from PIL import Image
 
 def main():
-    weights = np.loadtxt('weights.csv',delimiter=',')
-    biases = np.loadtxt('biases.csv',delimiter=',')
+    weights_1 = np.loadtxt('weights_dp_1.csv',delimiter=',')
+    biases_1 = np.loadtxt('biases_dp_1.csv',delimiter=',')
+    weights_2 = np.loadtxt('weights_dp_2.csv',delimiter=',')
+    biases_2 = np.loadtxt('biases_dp_2.csv',delimiter=',')
 
     imageName = input("Please enter the path of the image: ")
     myImage = Image.open(imageName)
@@ -13,9 +15,14 @@ def main():
         for j in data[i]:
             myImage.append(j)
     myImage = np.array(myImage)
-    temp = np.dot(np.transpose(myImage),np.transpose(weights)) + biases
+    
+    temp = np.dot(np.transpose(myImage),np.transpose(weights_1)) + biases_1
     temp = sigmoid(temp)
-    res = np.argmax(temp)
+    temp = np.dot(temp,np.transpose(weights_2)) + biases_2
+    temp = sigmoid(temp)
+
+    res = np.argmax(temp[0])
+
     print(chr(res + 65))
 
 def sigmoid(x):
